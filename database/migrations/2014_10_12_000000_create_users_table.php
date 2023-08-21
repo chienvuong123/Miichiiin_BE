05d220692976c0d894578bc67e0c5cfc53bdbecd
Nguyen Tien Tung <ntt091103@gmail.com>
2023-08-21 16:36:21 +0700

    fake data users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,16 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
+            $table->string('description');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('status');
+            $table->string('gender');
+            $table->date('date');
             $table->timestamps();
         });
     }
