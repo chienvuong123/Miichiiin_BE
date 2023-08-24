90695e2e9f262a6c6aa4ee21caf0f23ac13a30a9
Nguyen Tien Tung <ntt091103@gmail.com>
2023-08-24 23:12:36 +0700

    HM-47 done API CRUD Service

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text(20),
+            'description' => fake()->text(20),
+            'quantity' => fake()->numerify,
+            'price' => fake()->numerify,
+            'image' => fake()->imageUrl,
+            'status' => fake()->numberBetween(0,2),
+        ];
+    }
+}
