45f8aa6e865a29ad1267e3d45b29576dc8d8ec3e
Nguyen Tien Tung <ntt091103@gmail.com>
2023-10-11 00:31:19 +0700

    change migration

--- database/migrations/2023_08_20_085402_create_category_rooms_table.php
+++ database/migrations/2023_08_20_085402_create_category_rooms_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('category_rooms', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_hotel')->nullable();
             $table->string('name')->nullable();
             $table->string('description')->nullable();
             $table->string('image')->nullable();
