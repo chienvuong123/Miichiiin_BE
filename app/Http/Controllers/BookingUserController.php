ea0ff1ad754d9b8216639a46d7805e00da356a6f
Nguyen Tien Tung <ntt091103@gmail.com>
2023-10-25 23:00:47 +0700

    fix booking user

--- app/Http/Controllers/BookingUserController.php
+++ app/Http/Controllers/BookingUserController.php
@@ -10,6 +10,8 @@ class BookingUserController extends Controller {
         // Create Booking
         $id_user = Auth::user()->id ?? null;
         $data = $request->except('_token');
-        return create_booking($data["id_hotel"], $data, $id_user);
+        $booking = create_booking($data["id_hotel"], $data, $id_user);
+        $response = get_detail_booking($booking["message"]["id"]);
+        return response()->json($response["message"], $response["status"]);
     }
 }
